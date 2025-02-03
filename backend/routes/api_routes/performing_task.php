5d1a3691f6f89692c7758d828a4fdcb376cdf97b
Artem Moiseev <aamoiseev09@gmail.com>
2025-02-03 19:39:10 +0300

    add middleware to performing_task

--- backend/routes/api_routes/performing_task.php
+++ backend/routes/api_routes/performing_task.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(PerformingTaskController::class)
 ->prefix('/user/tasks')
+->middleware('auth:sanctum')
 ->group(function () {
     Route::get('/', 'index')->name('performing_task.index');
     Route::post('/', 'store')->name('performing_task.post')->middleware(UnStartedTaskMiddleware::class);
